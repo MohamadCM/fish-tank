8c0738786c41ae993d3b2e834d83cdfa8ad01bda
Mohamad <m.chamanmotlagh@gmail.com>
2024-12-19 18:30:44 +0100

    Add movement for lamp

--- src/fish_tank/lamp.cpp
+++ src/fish_tank/lamp.cpp
@@ -6,7 +6,6 @@
 #include <shaders/advanced_material_frag_glsl.h>
 
 
-
 #include <shaders/diffuse_vert_glsl.h>
 #include <shaders/diffuse_frag_glsl.h>
 
@@ -17,11 +16,15 @@ std::unique_ptr<ppgso::Texture> Lamp::baseColor;
 std::unique_ptr<ppgso::Texture> Lamp::metallicRoughness;
 std::unique_ptr<ppgso::Texture> Lamp::normalMap;
 
-Lamp::Lamp() {
+Lamp::Lamp()
+{
     if (!shader) shader = std::make_unique<ppgso::Shader>(advanced_material_vert_glsl, advanced_material_frag_glsl);
-    if (!baseColor) baseColor = std::make_unique<ppgso::Texture>(ppgso::image::loadBMP("textures/desk-light_baseColor.bmp"));
-    if (!metallicRoughness) metallicRoughness = std::make_unique<ppgso::Texture>(ppgso::image::loadBMP("textures/desk-light_metallicRoughness.bmp"));
-    if (!normalMap) normalMap = std::make_unique<ppgso::Texture>(ppgso::image::loadBMP("textures/desk-light_normal.bmp"));
+    if (!baseColor) baseColor = std::make_unique<ppgso::Texture>(
+        ppgso::image::loadBMP("textures/desk-light_baseColor.bmp"));
+    if (!metallicRoughness) metallicRoughness = std::make_unique<ppgso::Texture>(
+        ppgso::image::loadBMP("textures/desk-light_metallicRoughness.bmp"));
+    if (!normalMap) normalMap = std::make_unique<ppgso::Texture>(
+        ppgso::image::loadBMP("textures/desk-light_normal.bmp"));
     if (!mesh) mesh = std::make_unique<ppgso::Mesh>("lamp.gltf");
 
     scale = glm::vec3(0.04f, 0.04f, 0.04f);;
@@ -30,9 +33,17 @@ Lamp::Lamp() {
     rotation.y = glm::radians(45.0f);
 }
 
-bool Lamp::update(Scene &scene, float dt) {
+bool Lamp::update(Scene& scene, float dt)
+{
+    // Accumulate time
+    elapsedTime += dt;
+
+    // Oscillate position on the X-axis using sine wave
+    position.x = -8.0f + sin(elapsedTime * 2.0f) * 1.5f;
+
     // Update the light position (top of the lamp)
-    lightPosition = position + glm::vec3(scale.x * 5.0f, -scale.y * 5.0f, scale.z * 5.0f); // Adjust Y-offset as per lamp's height.
+    lightPosition = position + glm::vec3(scale.x * 5.0f, -scale.y * 5.0f, scale.z * 5.0f);
+    // Adjust Y-offset as per lamp's height.
 
     // Add this lamp's light source to the scene
     scene.lightSources.push_back(lightPosition);
@@ -42,7 +53,8 @@ bool Lamp::update(Scene &scene, float dt) {
     return true;
 }
 
-void Lamp::render(Scene &scene) {
+void Lamp::render(Scene& scene)
+{
     // Use the shader
     shader->use();
 
